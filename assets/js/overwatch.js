a1c52f085b30d2929c655ffd989df596bf1b6c7d
Anthony Plummer <aplummer@psda.com>
2016-06-03 00:57:35 -0400

    Figured it out. bootstrap offset syntax == 'col-md-offset-#'

--- assets/js/overwatch.js
+++ assets/js/overwatch.js
@@ -33,8 +33,8 @@ $(document).ready(function() {
   }
 
   var row = '<div class="row"></div>';
-  var greeting =  '<div id="greeting" class="col-md-4 col-offset-4 col-centered">' +
-                  '<p>Welcome ' + name + '</p>'
+  var greeting =  '<div id="greeting" class="col-md-4 col-md-offset-4 col-centered">' +
+                  '<p>Welcome ' + name + '</p>' +
                   '</div>';
   var txt1 =  '<div class="col-md-8">' +
               '<p>This is the <u>OverWatch</u> program. Simplistic in nature, cell-splitting accuracy, lightweight, portable, and scalable enough to map the entire enterprise column, end-to-end.</p>' +
@@ -43,7 +43,7 @@ $(document).ready(function() {
   if (scene01) {
       console.log("Awesome job! You've made it here. Enter the PIN Anthony gave you:");
   }
-  $("#scene01").addClass("scene");
+  $("#scene01").addClass("scene fluid-container");
   $("#scene01").append(row);
   $("#scene01 .row").append(greeting);
 
