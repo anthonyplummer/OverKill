dc82df0239b8503fac702fdc0e3fa8cc3c797f9e
Anthony Plummer <aplummer@psda.com>
2016-07-02 00:53:54 -0400

    Clean stack.. Green for Go

--- assets/test/js/controllers/HomeController.js
+++ assets/test/js/controllers/HomeController.js
@@ -8,8 +8,9 @@ app.controller('SceneController', function() {
   //Collect scenes
   var scene = document.getElementsByClassName("scene");
 
-  var greeting =  '<div id="greeting" class="col-md-3 col-md-offset-1">' +
-                '<p>Welcome</p>' +
+  var greeting =  '<div id="greeting" class="col-md-3 col-md-offset-2">' +
+                '<h1>Anthony T. Plummer</h1>' +
+                '<h2>Enterprise Web Architecht</h2>' +
                 '</div>';
   var txt1 =  '<div class="col-md-8">' +
               '<p>This is the <u>OverWatch</u> program. Simplistic in nature, cell-splitting accuracy, lightweight, portable, and scalable enough to map the entire enterprise column, end-to-end.</p>' +
@@ -36,14 +37,67 @@ app.controller('SceneController', function() {
   // Initiate ScrollMagic
   var controller = new ScrollMagic.Controller();
 
-  // create scene for every scene
-  for (i=0; i<scene.length; i++) {
-  		new ScrollMagic.Scene({
-  		    triggerElement: scene[i],
-          triggerHook: "onLeave"
-  			})
-  			.setPin(scene[i])
-  			.addIndicators() // add indicators (requires plugin)
-  			.addTo(controller);
-  };
+  // Scene 0
+  new ScrollMagic.Scene({
+    triggerElement: scene[0],
+    triggerHook: "onLeave"
+  })
+  .setPin(scene[0])
+  .addIndicators()
+  .addTo(controller);
+
+  // Scene 1
+  new ScrollMagic.Scene({
+    triggerElement: scene[1],
+    duration: "2000",
+    triggerHook: "onLeave"
+  })
+  .setPin(scene[1])
+  .addIndicators()
+  .addTo(controller);
+
+  // Scene 2
+  new ScrollMagic.Scene({
+    triggerElement: scene[2],
+    triggerHook: "onLeave"
+  })
+  .setPin(scene[2])
+  .addIndicators()
+  .addTo(controller);
+
+  // Scene 3
+  new ScrollMagic.Scene({
+    triggerElement: scene[3],
+    triggerHook: "onLeave"
+  })
+  .setPin(scene[3])
+  .addIndicators()
+  .addTo(controller);
+
+  // Scene 4
+  new ScrollMagic.Scene({
+    triggerElement: scene[4],
+    triggerHook: "onLeave"
+  })
+  .setPin(scene[4])
+  .addIndicators()
+  .addTo(controller);
+
+  // Scene 5
+  new ScrollMagic.Scene({
+    triggerElement: scene[5],
+    triggerHook: "onLeave"
+  })
+  .setPin(scene[5])
+  .addIndicators()
+  .addTo(controller);
+
+  // Scene 6
+  new ScrollMagic.Scene({
+    triggerElement: scene[6],
+    triggerHook: "onLeave"
+  })
+  .setPin(scene[6])
+  .addIndicators()
+  .addTo(controller);
 });
